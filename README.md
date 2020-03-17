b51c72aef49a65891c9de4cef86b1cb5840eb7d1
Adam Hooper <adam@adamhooper.com>
2020-03-17 09:37:10 -0400

    Document Buffer/String and null/undefined details
    Also, bump deps. (The new deps help with the tests.)

--- README.md
+++ README.md
@@ -24,37 +24,63 @@ var tree = new TernaryBufferTree(new Buffer('bar\nbaz\nfoo\nmoo\nthe foo', 'utf-
 console.log(tree.contains('foo')); // true
 console.log(tree.contains('moo')); // false
 
-// Super-optimized method that scratches an itch we had
-console.log(tree.findAllMatches('the foo drove over the moo', 2)); // [ 'the foo', 'foo', 'moo' ]
-
 // Use as a map. Input is a single Buffer: tabs separate keys from values.
 //
-// Performance will be best if the keys are sorted in UTF-8 byte order.
-var tree = new TernaryBufferTree(new Buffer('foo\tFOO\nbar\tBAR\nbaz\tBAZ', 'utf-8'));
+// When a key is queried without a value, `.get()` returns `null`. When a key
+// does not exist, `.get()` returns `undefined`.
+//
+// Performance will be best if input keys are sorted in UTF-8 byte order.
+var tree = new TernaryBufferTree(new Buffer('foo\tFOO\nbar\tBAR\nbaz\tBAZ\nx\ny', 'utf-8'));
 
 console.log(tree.get('foo')); // 'FOO'
+console.log(tree.get('x')); // null -- exists but has no value
 console.log(tree.get('moo')); // undefined
+
+// Super-optimized method that scratches an itch we had
+//
+// You can insert multi-word tokens and then query for single-word or
+// multi-word combinations. The dataset needn't have any values; the query
+// words must be space-separated.
+var tree = new TernaryBufferTree(new Buffer('bar\nbaz\nfoo\nmoo\nthe foo', 'utf-8'));
+console.log(tree.findAllMatches('the foo drove over the moo', 2)); // [ 'the foo', 'foo', 'moo' ]
 ```
 
-findAllMatches
---------------
+contains(key)
+-------------
+
+`.contains(key)` is equivalent to `.get(key) !== undefined`.
+
+`key` may be a `String` or a UTF-8 `Buffer`. `Buffer` logic should be more
+efficient, as `String` input will be encoded as UTF-8 internally.
+
+get(key)
+--------
+
+You can use TernaryBufferTree as a set or as a map. The input Buffer always
+contains one key per line, but if you add a tab and then a value, the `get()`
+method will return the given value for the given key.
+
+If the key was provided without a value, `.get(key)` will return `null`. If
+the key was not provided, `.get(key)` will return `undefined`.
+
+`key` may be a `String` (and `.get(key)` will return a `String`); or it may be
+a UTF-8 `Buffer` (and `.get(key)` will return a `Buffer`). `String` types imply
+UTF-8 encoding and decoding; `Buffer` types are more direct because the
+tree data is stored as UTF-8.
+
+findAllMatches(tokens, maxNgramSize)
+------------------------------------
 
 This method is interesting in that it can search for tokens that span multiple
 words (the second argument specifies the number of words), in a memory-efficient
 manner. The memory used is the size of the output Array. The time complexity is
 on the order of the size of the input times the number of tokens.
 
-get
----
-
-You can use TernaryBufferTree as a set or as a map. The input Buffer always
-contains one key per line, but if you add a tab and then a value, the `get()`
-method will return the given value for the given key.
-
-If the given key has no value (i.e., there is no tab), then `get(key)` will
-return `undefined`. In other words: `undefined` means either the key does not
-exist, or the key has no value. Use `contains()` to determine which is the
-case.
+`tokens` may be a `String` (and `.findAllMatches(tokens, n)` will return an
+`Array` of `String`s); or it may be a UTF-8 `Buffer` (and
+`.findAllMatches(tokens, n)` will return an `Array` of `Buffer`s). `String`
+types imply UTF-8 encoding and decoding; `Buffer` types are more direct because
+the tree data is stored as UTF-8.
 
 Developing
 ----------
--- test/index-test.js
+++ test/index-test.js
@@ -1,50 +1,86 @@
-var Set = require('../index');
-var expect = require('chai').expect;
+const Set = require('../index');
+const expect = require('chai').expect;
 
 describe('TernaryBufferTree', function() {
-  it('should return true/false from tests appropriately', function() {
-    var set = new Set(Buffer.from('foo\nbar\nbaz\nthe foo', 'utf-8'));
+  it('should return true/false from .contains() with a Buffer argument', function() {
+    const set = new Set(Buffer.from('foo\nbar\nbaz\ncafé\nthe foo', 'utf-8'));
     expect(set.contains(Buffer.from('foo', 'utf-8'))).to.be.true;
     expect(set.contains(Buffer.from('the foo', 'utf-8'))).to.be.true;
+    expect(set.contains(Buffer.from('café', 'utf-8'))).to.be.true;
     expect(set.contains(Buffer.from('moo', 'utf-8'))).to.be.false;
     expect(set.contains(Buffer.from('fooX', 'utf-8'))).to.be.false;
   });
 
-  it('should allow testing for Strings, which are encoded as utf-8', function() {
-    var set = new Set(Buffer.from('foo\nbar\nbaz\nthe foo', 'utf-8'));
+  it('should return true/false from .contains() with a String argument', function() {
+    const set = new Set(Buffer.from('foo\nbar\nbaz\ncafé\nthe foo', 'utf-8'));
     expect(set.contains('foo')).to.be.true;
     expect(set.contains('the foo')).to.be.true;
+    expect(set.contains('café')).to.be.true;
     expect(set.contains('moo')).to.be.false;
     expect(set.contains('fooX')).to.be.false;
   });
 
   it('should return a String Array of found tokens', function() {
-    var set = new Set(Buffer.from('foo\nbar\nbaz\nthe foo\nmoo', 'utf-8'));
+    const set = new Set(Buffer.from('foo\nbar\nbaz\ncafé\nthe foo\nmoo', 'utf-8'));
 
-    expect(set.findAllMatches('the foo went over the moo', 2))
-      .to.deep.eq([ 'the foo', 'foo', 'moo' ]);
+    expect(set.findAllMatches('café the foo went over the moo', 2))
+      .to.deep.eq([ 'café', 'the foo', 'foo', 'moo' ]);
 
-    expect(set.findAllMatches('the foo went over the moo', 1))
-      .to.deep.eq([ 'foo', 'moo' ]);
+    expect(set.findAllMatches('café the foo went over the moo', 1))
+      .to.deep.eq([ 'café', 'foo', 'moo' ]);
   });
 
-  it('should return `undefined` from .get() when there are no values', function() {
-    var set = new Set(Buffer.from('foo\nbar\nbaz', 'utf-8'));
-    expect(set.get('foo')).not.to.be.defined;
-    expect(set.get('moo')).not.to.be.defined;
-    // In other words: nothing is defined
+  it('should return a Buffer Array of found tokens', function() {
+    const set = new Set(Buffer.from('foo\nbar\nbaz\ncafé\nthe foo\nmoo', 'utf-8'));
+
+    expect(set.findAllMatches(Buffer.from('café the foo went over the moo'), 2))
+      .to.deep.eq([ 'café', 'the foo', 'foo', 'moo' ].map(Buffer.from));
+
+    expect(set.findAllMatches(Buffer.from('café the foo went over the moo'), 1))
+      .to.deep.eq([ 'café', 'foo', 'moo' ].map(Buffer.from));
+  });
+
+  it('should return `null` from .get() when there are no values', function() {
+    const set = new Set(Buffer.from('foo\nbar\nbaz', 'utf-8'));
+    expect(set.get('foo')).to.be.null; // it exists but has no value
+    expect(set.get('bar')).to.be.null;
+    expect(set.get('baz')).to.be.null;
+    expect(set.get('aoo')).to.be.undefined;
+    expect(set.get('coo')).to.be.undefined;
+    expect(set.get('moo')).to.be.undefined;
+    expect(set.get(Buffer.from('foo'))).to.be.null; // it exists but has no value
+    expect(set.get(Buffer.from('bar'))).to.be.null;
+    expect(set.get(Buffer.from('baz'))).to.be.null;
+    expect(set.get(Buffer.from('aoo'))).to.be.undefined;
+    expect(set.get(Buffer.from('coo'))).to.be.undefined;
+    expect(set.get(Buffer.from('moo'))).to.be.undefined;
   });
 
   it('should return values from `.get()` when there are values', function() {
-    var set = new Set(Buffer.from('foo\tFOO\nbar\t\nbaz\tBAZ', 'utf-8'));
+    const set = new Set(Buffer.from('foo\tFOO\nbar\t\nbaz\tBAZ\nmoo\nmar', 'utf-8'));
     expect(set.get('foo')).to.eq('FOO');
     expect(set.get('bar')).to.eq('');
-    expect(set.get('moo')).not.to.be.defined;
+    expect(set.get('baz')).to.eq('BAZ');
+    expect(set.get('moo')).to.be.null;
+    expect(set.get('mar')).to.be.null;
+    expect(set.get('aoo')).to.be.undefined;
+    expect(set.get('bat')).to.be.undefined;
+    expect(set.get('coo')).to.be.undefined;
+    expect(set.get('zoo')).to.be.undefined;
+    expect(set.get(Buffer.from('foo'))).to.deep.eq(Buffer.from('FOO'));
+    expect(set.get(Buffer.from('bar'))).to.deep.eq(Buffer.from(''));
+    expect(set.get(Buffer.from('baz'))).to.deep.eq(Buffer.from('BAZ'));
+    expect(set.get(Buffer.from('moo'))).to.be.null;
+    expect(set.get(Buffer.from('mar'))).to.be.null;
+    expect(set.get(Buffer.from('aoo'))).to.be.undefined;
+    expect(set.get(Buffer.from('bat'))).to.be.undefined;
+    expect(set.get(Buffer.from('coo'))).to.be.undefined;
+    expect(set.get(Buffer.from('zoo'))).to.be.undefined;
   });
 
   it('should reject zero-length strings', function() {
-    var set = new Set(Buffer.from('foo\n'));
-    expect(set.contains('foo')).to.be.true;
+    const set = new Set(Buffer.from('foo\n'));
     expect(set.contains('')).to.be.false;
+    expect(set.contains(Buffer.from(''))).to.be.false;
   });
 });
